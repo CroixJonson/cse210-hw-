7cb6072d57614352442ea9182a2f6c3e8e577750
CroixJonson <croixj02@gmail.com>
2026-01-28 15:56:32 -0700

    journal and entry project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+
+        List<string> prompts = new List<string>()
+        {
+         "Who was the most interesting person I interacted with today?",
+         "What was the best part of my day?",
+         "How did I see the hand of the Lord in my life today?",
+         "What was the strongest emotion I felt today?",
+         "If I had one thing I could do over today, what would it be?"
+        };
+
+        Random rand = new Random();
+
+        string choice = "";
+
+        // menu parts
+
+        while (choice != "5")
+        {
+            Console.WriteLine("1. Write new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save journal");
+            Console.WriteLine("4. Load journal");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            choice = Console.ReadLine();
+
+            //menu choice one
+
+            if (choice == "1")
+            {
+                string prompt = prompts[rand.Next(prompts.Count)];
+                Console.WriteLine(prompt);
+                string response = Console.ReadLine();
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._prompt = prompt;
+                entry._response = response;
+                journal.AddEntry(entry);
+            }
+            // menu choice two
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+
+            // choice three
+
+            else if (choice == "3")
+            {
+                Console.WriteLine("Please input a filename: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+
+            // choice four
+            else if (choice == "4")
+            {
+                Console.WriteLine("What file would you like to open: ");
+                string loadfile = Console.ReadLine();
+                journal.LoadFromFile(loadfile);
+            }
+
+            else if (choice == "5")
+            {
+                Console.WriteLine("Complete!");
+            }
+
+        }
+        
     }
 }
+
---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,30 @@
+public class Entry
+{
+    public string _date;
+    public string _prompt;
+    public string _response;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_date}");
+        Console.WriteLine($"Prompt: {_prompt}");
+        Console.WriteLine($"Response: {_response}");
+        Console.WriteLine();
+    }
+    public string ToFileString()
+    {
+        return $"{_date},{_prompt},{_response}";
+    }
+
+    public static Entry FromFileString(string line)
+    {
+        string[] parts = line.Split(",");
+
+        Entry entry = new Entry();
+        entry._date = parts[0];
+        entry._prompt = parts[1];
+        entry._response = parts[2];
+
+        return entry;
+    }
+}
