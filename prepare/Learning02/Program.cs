9f14139b710434c8968f259f03bd98a8dc69898b
CroixJonson <croixj02@gmail.com>
2026-01-28 16:28:39 -0700

    learningact

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Cybersecurity analyst";
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job1._company = "apple";
+
+        job1.Display();
+        job2.Display();
+
+
+        Resume myResume = new Resume();
+        myResume._membername = "Croix Jonson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
